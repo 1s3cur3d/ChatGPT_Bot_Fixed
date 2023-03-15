e938468e96913f03951090c12fb3312728e12723
mr-wh1tehat <clonescrt@mail.ru>
2023-03-15 09:48:57 +0500

    Attempt to fix bot not replying

--- chat_gpt_bot/config.py
+++ chat_gpt_bot/config.py
@@ -1,21 +1,21 @@
 import os
 
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv(BOT_TOKEN)
 
-RAILWAY_APP_NAME = os.getenv('RAILWAY_APP_NAME')
+RAILWAY_APP_NAME = os.getenv(RAILWAY_APP_NAME)
 
 WEBHOOK_HOST = f'https://{RAILWAY_APP_NAME}.railway.app'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_PORT = os.getenv(PORT, default=8000)
 
-WORKS_CHATS = [os.getenv(CHAT_ID)]
+CHAT_ID = [os.getenv(CHAT_ID)]
 
 # AI
-AI_KEY = os.getenv('AI_KEY')
+AI_KEY = os.getenv(AI_KEY)
 MODEL = 'text-davinci-003'
 TEMPERATURE = 0.5
 MAX_TOKENS = 1000
